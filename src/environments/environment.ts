f4d10b006ff530f5dae94a7fccf00f06477297bd
disto-stack <juan.ocampo.2501@gmail.com>
2021-05-12 23:03:39 -0500

    AngularFire configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCRFpTOtWs9DbBhjJSDK69WSjWN8uZG9rk',
+    authDomain: 'chemistry-b48c4.firebaseapp.com',
+    projectId: 'chemistry-b48c4',
+    storageBucket: 'chemistry-b48c4.appspot.com',
+    messagingSenderId: '1012434426012',
+    appId: '1:1012434426012:web:360dffe8457ea207737559',
+    measurementId: 'G-LCKSWL7QVD'
+  }
 };
 
 /*
