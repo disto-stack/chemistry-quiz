3f09658c5d7b2010e89db5921323f214f9b89d6f
disto-stack <juan.ocampo.2501@gmail.com>
2021-05-13 16:41:48 -0500

    Home Page added and tailwind custom styles configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,31 @@ module.exports = {
     },
     darkMode: false, // or 'media' or 'class'
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          background: {
+            DEFAULT: '#34495e',
+            dark: '#2c3e50'
+          },
+          palette: {
+            primary: '#2ecc71',
+            dark: '#27ae60',
+            error: '#e74c3c'
+          },
+          podium: {
+            gold: '#c9b037',
+            silver: '#b4b4b4',
+            bronze: '#ad8a56'
+          }
+        },
+        fontFamily: {
+          sourceCode: ['Source Code Pro', 'monospace']
+        },
+        width: {
+          mobile: '280px',
+          tablet: '380px'
+        }
+      },
     },
     variants: {
       extend: {},
