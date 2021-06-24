20a11d4f38e5bf79cb9019d563d60c1b56bebbe6
disto-stack <juan.ocampo.2501@gmail.com>
2021-06-23 19:01:04 -0500

    Deployed in firestore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCRFpTOtWs9DbBhjJSDK69WSjWN8uZG9rk',
+    authDomain: 'chemistry-b48c4.firebaseapp.com',
+    projectId: 'chemistry-b48c4',
+    storageBucket: 'chemistry-b48c4.appspot.com',
+    messagingSenderId: '1012434426012',
+    appId: '1:1012434426012:web:360dffe8457ea207737559',
+    measurementId: 'G-LCKSWL7QVD'
+  }
 };
